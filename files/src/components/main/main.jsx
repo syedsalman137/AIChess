1c79cd2ecb7256d8b94ac74bad028c8b5364a995
Syed Salman Habeeb Quadri <98826220+SalmanHabeeb@users.noreply.github.com>
2022-12-26 12:21:25 +0000

    Update UI

--- files/src/components/main/main.jsx
+++ files/src/components/main/main.jsx
@@ -3,7 +3,14 @@ import './main.css';
 function MainPage() {
     return (
     <div className='main'>
-        <a className='play-link' href='/arena'><button className='play-button'>Click to Play</button></a>
+        <div className='link-box'>
+            <div className='text-cont'>
+                <div className='title'>AIChess</div>
+                <div className='info'>A web based chess engine</div>
+            </div>
+            <div style={{backgroundImage: `url(${"assets/images/hq-chess-pieces-transparent-hd-photo-29.png"})`}}  className='bgimage'></div>
+        </div>
+        <a className='play-link' href='/arena'><button className="play-button" role="button"><span className="text">Play the AI</span></button></a>
     </div>
     )
 }
--- first.py
+++
@@ -1,3 +0,0 @@
-import re
-
-print(dir(re))
